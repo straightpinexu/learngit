c81d5fcee8d28c9251b33b2eaa011567484ce658
pinexu666 <straightpinexu@outlook.com>
2020-03-10 11:24:42 +0800

    add method len

--- test.py
+++ test.py
@@ -6,7 +6,11 @@ class Pinexu():
     def __init__(self, args):
         self.id = 1
     
-    def __getitem__(id):
+    def __getitem__(self, id):
         return self.id
         666
+     
+    def __len__(self):
+        return 66
+
         
